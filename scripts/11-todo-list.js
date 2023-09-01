78ebe679124171aa69464554cdb94061bfbaf48b
Onwe Emeka <emekaonwed@gmail.com>
2023-09-01 18:14:43 +0100

    finished adding scripting to the todo list

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,4 +1,10 @@
-const todoList = ['make dinner', 'wash dishes'];
+const todoList = [{
+    name:'make dinner',
+    dueDate: '2022-12-22'
+ }, {
+    name: 'wash dishes',
+    dueDate: '2022-12-22'
+}];
 
 renderTodoList();
 
@@ -6,8 +12,16 @@ function renderTodoList(){
     let todoListHTML = '';
 
     for(let i = 0; i < todoList.length; i++) {
-        const todo = todoList[i];
-        const html = `<p> ${todo}</p>`;
+        const todoObject = todoList[i];
+        const name = todoObject.name;
+        const dueDate = todoObject.dueDate;
+        const html = `<p> 
+        ${name} ${dueDate} 
+        <button onclick="
+            todoList.splice(${i},1);
+            renderTodoList();
+        ">Delete</button>
+        </p>`;
         todoListHTML +=  html;
     }
 
@@ -22,9 +36,16 @@ function addTodo(){
     const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value;
 
+    const dateInputElement = document.querySelector('.js-due-date-input');
 
-    todoList.push(name);
-    console.log(todoList);
+const dueDate = dateInputElement.value;
+
+todoList.push({
+    name: name,
+    dueDate: dueDate
+})
+
+
 
     inputElement.value = ''; // makes the textbox empty
 
